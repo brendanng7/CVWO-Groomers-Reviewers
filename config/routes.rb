895f0f8c24c599b025a80ea493448d3e1e0413b9
brendanng7 <brendanngjj@gmail.com>
2024-01-24 22:16:53 +0800

    Setup Backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+  devise_for :users,
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get '/member-data', to: 'members#show'
+
+  namespace :api do
+    namespace :v1 do
+      get 'comments/index'
+      get 'comments/create'
+      get 'comments/show'
+      get 'comments/destroy'
+
+      get 'reviews/index'
+      post 'reviews/create'
+      get '/show/:id', to: 'reviews#show'
+      delete '/destroy/:id', to: 'users#destroy'
+    end
+  end
+
+  root 'home#index'
+  get '/*path' => 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
